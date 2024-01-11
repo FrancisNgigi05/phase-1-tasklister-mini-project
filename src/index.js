c75af28c4cd707b83c4fa0c6226471bd235678ce
FrancisNgugi05 <francis.ngugi@student.moringaschool.com>
2024-01-11 07:35:40 +0300

    almost completed asssignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function () {
+	const tasklist = document.querySelector('#tasks');
+	const submit = document.querySelector('#create-task-form');
+	submit.addEventListener('submit', function (event) {
+		event.preventDefault();
+
+		const input = document.querySelector('input#new-task-description');
+
+		const taskDescription = input.value;
+
+		// create a new ul element
+		const newTask = document.createElement('li');
+
+		// the text content to be set at the li element
+		newTask.textContent = taskDescription;
+
+		
+		// creating the delete button
+		const deleteButton = document.createElement('button');
+		deleteButton.textContent = "x";
+		
+		deleteButton.addEventListener('click', function () {
+			tasklist.removeChild(newTask);
+		});
+		// append the delete button to the li element
+		newTask.appendChild(deleteButton);
+
+		// append the li element to the ul element
+		tasklist.appendChild(newTask);
+	
+		// clear the input field after adding the task
+		input.value = '';
+	});
 });
